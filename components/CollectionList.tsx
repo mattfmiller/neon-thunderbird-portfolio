13eb966c936f46223659452263477e872c35629f
Matt Miller <matt.f.miller@live.com>
2021-12-24 16:28:21 -0800

    feat: change background on hover over category

--- components/CollectionList.tsx
+++ components/CollectionList.tsx
@@ -1,41 +1,69 @@
 import styled from "@emotion/styled";
 import Link from "next/link";
-import React from "react";
-import { Collection } from "../constants/constants";
+import React, { useState } from "react";
+import { Collection, CollectionCoverPhotos } from "../constants/constants";
 
-const Div = styled.div({
-  alignItems: "center",
-  backgroundAttachment: "fixed",
-  backgroundImage: "url('/bgImage.jpg')",
-  backgroundPosition: "center",
-  backgroundRepeat: "no-repeat",
-  backgroundSize: "cover",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  minHeight: "100vh",
-  minWidth: "100vw",
-});
+const DEFAULT_BG_PATH = "/bgImage.jpg";
+
+const Div = styled.div(
+  {
+    alignItems: "center",
+    backgroundAttachment: "fixed",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundColor: "rgba(255,255,255,0.5)",
+    backgroundBlendMode: "lighten",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    minHeight: "100vh",
+    minWidth: "100vw",
+    transition: "0.3s ease all",
+  },
+  (props) => ({
+    backgroundImage: `url(${props.path})`,
+  })
+);
 
 const CollectionLink = styled.a({
   margin: 10,
 });
 
 const CollectionList: React.FC = () => {
+  const [path, setPath] = useState(DEFAULT_BG_PATH);
   const collections = Object.keys(Collection);
   console.log(collections);
+
+  const handleMouseEnter = (collection: Collection) => {
+    setPath(CollectionCoverPhotos[Collection[collection]]);
+    console.log(collection, CollectionCoverPhotos[Collection[collection]]);
+  };
+  const handleMouseLeave = () => {
+    setPath(DEFAULT_BG_PATH);
+  };
   return (
-    <Div>
-      {collections.map((collection) => (
-        <Link
-          key={collection}
-          href="/collections/[id]"
-          as={`/collections/${collection}`}
-          passHref={true}
-        >
-          <CollectionLink>{collection}</CollectionLink>
-        </Link>
-      ))}
+    <Div path={path}>
+      {collections.map((collection) => {
+        if (!isNaN(parseInt(collection))) {
+          return null;
+        }
+        return (
+          <div
+            key={collection}
+            onMouseEnter={() => handleMouseEnter(collection)}
+            onMouseLeave={() => handleMouseLeave()}
+          >
+            <Link
+              href="/collections/[id]"
+              as={`/collections/${Collection[collection]}`}
+              passHref={true}
+            >
+              <CollectionLink>{collection}</CollectionLink>
+            </Link>
+          </div>
+        );
+      })}
     </Div>
   );
 };
